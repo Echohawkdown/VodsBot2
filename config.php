9bd239224cb6c940ce9ab502e42c0fd7e668e705
simon--poole <simon__poole@hotmail.com>
2014-01-12 18:28:32 +0000

    Repeat

--- config.php
+++ config.php
@@ -9,9 +9,5 @@ class Config {
 		"gg"=>"http://www.gosugamers.net/api/matches?apiKey=8d444a60d1c27e8113d2c28434f7b004&game=lol",
 		"googl"=>"AIzaSyB58png8MHNxijB5nc8dzbnB-8FrTlSt5Y",
 	);
-	static $Subreddit = array(
-		"url"=>"vod_bot_test",
-		"id"=>"t5_2zh95",
-	);
 }
 ?>
