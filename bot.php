ade1889f0b3a36443d6b69fafabf78948c3e769a
Echohawkdown <Echohawkdown@redd.it>
2014-02-15 10:42:43 -0800

    Added tag name converter & cleaned up code

--- bot.php
+++ bot.php
@@ -38,36 +38,28 @@ Class Bot {
 		if (!is_null($events)) {
 			foreach ($events['gg'] as $key => $value) $events['gg'][$key]['match_time'] = strtotime($value['datetime']);
  
-			// jD's API often returns NULL
-			if (isset($events['jd']) && !is_null($events['jd']) && $events['jd'] != 'NULL') {
-				$matches = array_merge($events['jd'], $events['gg']);
- 
-				usort($matches, function($key1, $key2) {
-					$value1 = $key1['match_time'];
-					$value2 = $key2['match_time'];
-					return $value2 - $value1;
-				});
-			} else {
-				usort($events['gg'], function($key1, $key2) {
-					$value1 = $key1['match_time'];
-					$value2 = $key2['match_time'];
-					return $value1 - $value2;
-				});
-				$matches = $events['gg'];
-			}
- 
-			return $matches;
+      usort($events['gg'], function($key1, $key2) {
+        $value1 = $key1['match_time'];
+        $value2 = $key2['match_time'];
+        return $value1 - $value2;
+      });
+      return $events['gg'];
 		}
 	}
  
 	public function parse($matches) {
+    // GosuGamer's tag system to Riot's tag system is in another file
+    // for easier editing & legibility
+    require 'team-tags.php';
 		$i = 0;
+    
 		foreach ($matches as $match) {
 			if ($i < $this->limit) {
 				// Time
 				$time = "";
 				$ticker[$i]['timestamp'] = $match['match_time'];
 				$total_time = time() - $match['match_time'];
+        // Should check out the following line - might be useful for a "recently played" ticker?
 				if ($total_time < 0) $total_time = $match['match_time'] - time();
 				$days       = floor($total_time / 86400);
 				$hours      = floor(($total_time % 86400) / 3600);
@@ -81,22 +73,26 @@ Class Bot {
 				$ticker[$i]['time'] = $time;
  
 				// tournament name
+        // Check if first case ever happens; may be legacy code from JoinDota API & /r/dota2
 				if (isset($match['coverage_title'])) $ticker[$i]['tournament'] = $match['coverage_title'];
 				else if (isset($match['tournament']['name'])) $ticker[$i]['tournament'] = $match['tournament']['name'];
-				$removes = array("-LoL",".LoL")
- 				$match['firstOpponent']['shortName'] = str_replace($removes, "", $match['firstOpponent']['shortName']);
- 				$match['secondOpponent']['shortName'] = str_replace($removes, "", $match['secondOpponent']['shortName']);
+				$removes = array("-LoL",".LoL"," LoL");
+ 				$match['firstOpponent']['shortName'] = str_ireplace($removes, "", $match['firstOpponent']['shortName']);
+ 				$match['secondOpponent']['shortName'] = str_ireplace($removes, "", $match['secondOpponent']['shortName']);
 				// Teams
-				if (isset($match['firstOpponent']['shortName']) && isset($match['secondOpponent']['shortName']))  $ticker[$i]['teams'] = '**'.$match['firstOpponent']['shortName'].'** vs **'.$match['secondOpponent']['shortName'].'**';
+				if (isset($match['firstOpponent']['shortName']) && isset($match['secondOpponent']['shortName'])){
+          // Will replace GosuGamer's short tags if it's in the $team_tags array at the beginning of this function
+          if (array_key_exists($match['firstOpponent']['shortName'], $team_tags))  $match['firstOpponent']['shortName'] = $team_tags[$match['firstOpponent']['shortName']];
+          if (array_key_exists($match['secondOpponent']['shortName'], $team_tags))  $match['secondOpponent']['shortName'] = $team_tags[$match['secondOpponent']['shortName']];
+          $ticker[$i]['teams'] = '**'.$match['firstOpponent']['shortName'].'** vs **'.$match['secondOpponent']['shortName'].'**';
+        }
  
 				// URLs
 				if (isset($match['pageUrl'])) $ticker[$i]['url']['gg'] = $match['pageUrl'];
-				//if (isset($match['coverage_url'])) $ticker[$i]['url']['jd'] = $match['coverage_url'];
-				if(strpos($ticker[$i]['url']['gg'], "playoff") !== false) $ticker[$i]['spoiler'] = true;
-				else if (strpos($ticker[$i]['url']['gg'], "top") !== false) $ticker[$i]['spoiler'] = true;
-				else if (strpos($ticker[$i]['url']['gg'], "Ro8") !== false) $ticker[$i]['spoiler'] = true;
-				else if (strpos($ticker[$i]['url']['gg'], "final") !== false) $ticker[$i]['spoiler'] = true;
-				else if (strpos($ticker[$i]['url']['gg'], "Final") !== false) $ticker[$i]['spoiler'] = true;
+				if (stripos($ticker[$i]['url']['gg'], "playoff") !== false) $ticker[$i]['spoiler'] = true;
+				else if (preg_match("/top ([0-9]+|\w+)/i", $ticker[$i]['url']['gg']) !== false) $ticker[$i]['spoiler'] = true;
+				else if (preg_match("/(ro([0-9]+|\w+))|(round of ([0-9]+|\w+))/i", $ticker[$i]['url']['gg']) !== false) $ticker[$i]['spoiler'] = true;
+				else if (stripos($ticker[$i]['url']['gg'], "final") !== false) $ticker[$i]['spoiler'] = true;
  				else $ticker[$i]['spoiler'] = false;
 				$i++;
 			} else break;
@@ -104,7 +100,7 @@ Class Bot {
  
 		return $ticker;
 	}
- 
+  
 	public function format($ticker) {
 		$tock = "";
 		foreach ($ticker as $tick) {
@@ -137,8 +133,8 @@ Class Bot {
 		$parameters['title'] = $data->title;
 		$parameters['public_description'] = $data->public_description;
 		$parameters['lang'] = $data->language;
-		$parameters['type'] = 'restricted';
-		$parameters['link_type'] = 'self';
+		$parameters['type'] = $data->subreddit_type;
+		$parameters['link_type'] = $data->content_options;
 		$parameters['wikimode'] = $data->wikimode;
 		$parameters['wiki_edit_karma'] = $data->wiki_edit_karma;
 		$parameters['wiki_edit_age'] = $data->wiki_edit_age;
@@ -159,11 +155,6 @@ Class Bot {
 		$this->snoopy->submit("http://www.reddit.com/api/site_admin?api_type=json", $parameters);
 	}
  
-	protected function gosugamers() {
-		$data = file_get_contents($this->API['gg']);
-		$data = json_decode($data);
-		return $data->matches;
-	}
 	private function shortenUrl($url){
 		$googl = new \Googl($this->API['googl']);
 	    $short = $googl->shorten($url);
---
+++ team-tags.php
@@ -0,0 +1,15 @@
+<?php
+  $team_tags = array(
+   "[A]" => "ALL",
+   "CHW" => "CW",
+   "Dignitas" => "DIG",
+   "Fnatic" => "FNC",
+   "GBG" => "GMB",
+   "LMQ iBP" => "LMQ Tc",
+   "[M]" => "MIL",
+   "ROCCAT" => "ROC",
+   "SHCXD" => "SHC",
+   "Team Coast" => "CST"
+   "LMQ iBP" => "LMQ Tc"
+  );
+?>
